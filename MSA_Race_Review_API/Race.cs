c61401fc3d493eb6b3349226342a26c13a78d905
GeorgeCLu <52149356+GeorgeCLu@users.noreply.github.com>
2024-07-30 21:29:23 +1200

    initial commit

---
+++ MSA_Race_Review_API/Race.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSA_Race_Review_API
+{
+    public class Race
+    {
+        public Race()
+        {
+            this.Review = new HashSet<Review>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int raceId { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The raceName value cannot exceed 50 characters. ")]  
+        public string raceName { get; set; }
+        [StringLength(50, ErrorMessage = "The championship value cannot exceed 50 characters. ")]  
+        public string championship { get; set; }
+        [Required]
+        public int year { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The track value cannot exceed 50 characters. ")]  
+        public string track { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The location value cannot exceed 50 characters. ")]  
+        public string location { get; set; }
+        public int averageScore { get; set; }
+        public int scoreSum { get; set; }
+        public int totalReviews { get; set; }
+        // public ICollection<Review> Review { get; set; }
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
---
+++ MSA_Race_Review_API/Review.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSA_Race_Review_API
+{
+    public class Review
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reviewId { get; set; }
+        [Required]
+        [StringLength(255, ErrorMessage = "The reviewtext value cannot exceed 255 characters. ")]  
+        public string reviewText { get; set; }
+        [Required]
+        public int? reviewScore { get; set; }
+        [Required]
+        [StringLength(25, ErrorMessage = "The reviewName value cannot exceed 25 characters. ")]  
+        public string reviewerName { get; set; }
+        public int upvotes { get; set; }
+        public DateTime timeCreated { get; set; }
+        [Required]
+        [ForeignKey("raceId")]
+        public int raceId { get; set; }
+    }
+}
